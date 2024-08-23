e100424c00b3ec02272ae37d2caa8dfcba91df82
jiamingkong <kinetical@live.com>
2024-08-24 03:52:50 +0800

    Turning on MPS

--- GPT_SoVITS/prepare_datasets/1-get-text.py
+++ GPT_SoVITS/prepare_datasets/1-get-text.py
@@ -52,8 +52,8 @@ if os.path.exists(txt_path) == False:
     os.makedirs(bert_dir, exist_ok=True)
     if torch.cuda.is_available():
         device = "cuda:0"
-    # elif torch.backends.mps.is_available():
-    #     device = "mps"
+    elif torch.backends.mps.is_available():
+        device = "mps"
     else:
         device = "cpu"
     if os.path.exists(bert_pretrained_dir):...
